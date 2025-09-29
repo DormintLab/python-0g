bbb3376232914c7abdadb1a6dba2b0915c789f3b
George Kasparyants <gg_plastt@mail.ru>
2025-09-29 13:47:07 +0400

    compile js bindings

--- tests/test_jsbindings.py
+++ tests/test_jsbindings.py
@@ -1,22 +1,5 @@
-import time
-from pathlib import Path
-import execjs
+from javascript import require
 
 
-ctx = execjs.compile(Path("./a0g/jsbindings/dist/bundle.js").read_text())
-
-# Ждём инициализацию
-timeout = 5.0  # секунд
-interval = 0.05
-elapsed = 0.0
-
-while not ctx.call("isReady"):
-    time.sleep(interval)
-    elapsed += interval
-    if elapsed > timeout:
-        raise TimeoutError("Pedersen hash initialization timed out")
-
-# Теперь можно безопасно вызывать
-msg = [0, 0]
-result = ctx.call("pedersenHashSync", msg)
-print(result)
+bundle = require("../a0g/jsbindings/dist/bundle.js")
+print(bundle.pedersenHash([0, 0]))

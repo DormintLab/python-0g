7831d36c3e42d11b9c615f86154456b8fb12bb39
George Kasparyants <gg_plastt@mail.ru>
2025-09-29 14:15:32 +0400

    openai headers

--- tests/test_jsbindings.py
+++ tests/test_jsbindings.py
@@ -1,5 +1,17 @@
+import os
+import sys
+
 from javascript import require
+import dotenv
+from a0g import A0G
+dotenv.load_dotenv()
+os.environ["REQ_TIMEOUT"] = "0"
 
-
+a0g = A0G()
+service = a0g.get_all_services()[1]
+print(service, file=sys.stderr)
 bundle = require("../a0g/jsbindings/dist/bundle.js")
-print(bundle.pedersenHash([0, 0]))
+
+private_key = os.environ.get("A0G_PRIVATE_KEY")
+print(bundle.getOpenAIHeadersDemo(private_key, "Dummy content", service.provider, a0g.rpc_url,
+                                  timeout=100000))
